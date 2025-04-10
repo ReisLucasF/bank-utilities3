003ace2fe5ea299445c3f0324f640d091b05a791
ReisLucasF <reislucasf@icloud.com>
2025-04-09 22:37:51 -0300

    FIX: Validação de resoluções

--- app/components/PopupCreator.jsx
+++ app/components/PopupCreator.jsx
@@ -126,6 +126,119 @@ const PopupCreator = () => {
     }
   };
 
+  // Função para verificar contraste entre cores
+  const verificarContrasteCores = (corFundo, corTexto, tipoTexto) => {
+    if (!corFundo || !corTexto) return true;
+    if (corFundo.toLowerCase() === corTexto.toLowerCase()) {
+      alert(
+        `A cor de fundo e a cor do ${tipoTexto} não podem ser iguais, pois o texto ficará invisível.`,
+      );
+      return false;
+    }
+    return true;
+  };
+
+  // Definir campos que não podem conter espaço
+  const camposComEspaco = {
+    corBordaCTA: "Cor da borda da CTA",
+    corFundoCTA: "Cor de fundo da CTA",
+    corFim: "Cor de fim do fundo",
+    corInicio: "Cor de início do fundo",
+    corTextoCTA: "Cor do texto da CTA",
+    corSubtitulo: "Cor do subtítulo",
+    corTitulo: "Cor do título",
+    corBtnFechar: "Cor do botão fechar",
+  };
+
+  // Função para validar formato hexadecimal de cores
+  const validarFormatoHex = (cor) => {
+    if (!cor || cor === "") return true; // Permitir vazio
+    if (!cor.startsWith("#") || cor.length !== 7) return false;
+    const hexRegex = /^#[0-9A-Fa-f]{6}$/;
+    return hexRegex.test(cor);
+  };
+
+  // Função para verificar o comprimento da cor e seu formato
+  const verificarComprimentoCor = (cor, nomeCampo) => {
+    if (cor.length === 0) return true;
+    if (!validarFormatoHex(cor)) {
+      alert(
+        `A cor do campo ${nomeCampo} deve ser um valor hexadecimal válido (ex: #FF0000). Por favor, corrija!`,
+      );
+      return false;
+    }
+    return true;
+  };
+
+  // Função para verificar a resolução da imagem com base no tipo de layout
+  const verificarResolucaoImagem = (imagem) => {
+    return new Promise((resolve, reject) => {
+      const img = new Image();
+
+      img.onload = () => {
+        if (tipoLayout === "335" && (img.width > 660 || img.height > 1267)) {
+          alert(
+            "A resolução da imagem para esse tipo de layout não pode ultrapassar 660x1267 pixels.",
+          );
+          setStatusArquivo(
+            "Erro: resolução da imagem muito alta para o layout 335",
+          );
+          setStatusArquivoCor("red");
+          reject(new Error("Resolução da imagem muito alta para o layout 335"));
+        } else if (
+          tipoLayout === "334" &&
+          (img.width > 500 || img.height > 500)
+        ) {
+          alert(
+            "A resolução da imagem para esse tipo de layout não pode ultrapassar 500x500 pixels.",
+          );
+          setStatusArquivo(
+            "Erro: resolução da imagem muito alta para o layout 334",
+          );
+          setStatusArquivoCor("red");
+          reject(new Error("Resolução da imagem muito alta para o layout 334"));
+        } else if (
+          tipoLayout === "333" &&
+          (img.width > 500 || img.height > 500)
+        ) {
+          alert(
+            "A resolução da imagem para esse tipo de layout não pode ultrapassar 500x500 pixels.",
+          );
+          setStatusArquivo(
+            "Erro: resolução da imagem muito alta para o layout 333",
+          );
+          setStatusArquivoCor("red");
+          reject(new Error("Resolução da imagem muito alta para o layout 333"));
+        } else {
+          // Verificar o tamanho do arquivo da imagem (limite de 100KB)
+          const limiteTamanhoBytes = 100 * 1024; // 100 KB em bytes
+          if (imagem.size > limiteTamanhoBytes) {
+            alert("O tamanho da imagem não pode ultrapassar 100KB.");
+            setStatusArquivo(
+              "Erro: tamanho da imagem excede o limite de 100KB",
+            );
+            setStatusArquivoCor("red");
+            reject(new Error("Tamanho da imagem excede o limite de 100KB."));
+          } else {
+            // Se a resolução e o tamanho estiverem dentro dos limites permitidos
+            resolve();
+          }
+        }
+      };
+
+      // Evento onerror para lidar com erros ao carregar a imagem
+      img.onerror = () => {
+        alert("Erro ao carregar a imagem.");
+        setStatusArquivo("Erro ao carregar a imagem");
+        setStatusArquivoCor("red");
+        reject(new Error("Erro ao carregar a imagem."));
+      };
+
+      // Carregar a imagem a partir do objeto Blob
+      img.src = URL.createObjectURL(imagem);
+    });
+  };
+
   // Definir qual preview de layout mostrar
   const getPopupPreview = () => {
     // Variáveis para tamanho de fonte baseado nos valores selecionados
@@ -152,7 +265,7 @@ const PopupCreator = () => {
             }}
           >
             <div className={styles.btnFechar} style={{ color: corBtnFechar }}>
-              {textoBtnFechar}
+              X {textoBtnFechar}
             </div>
             <div
               className={styles.popupImageTop}
@@ -198,7 +311,7 @@ const PopupCreator = () => {
             }}
           >
             <div className={styles.btnFechar} style={{ color: corBtnFechar }}>
-              {textoBtnFechar}
+              X {textoBtnFechar}
             </div>
             <div className={styles.popupContent}>
               <div
@@ -284,135 +397,162 @@ const PopupCreator = () => {
     setStatusArquivoCor("blue");
   };
 
-  // Gerar o script
-  const gerarScript = () => {
-    // Validações básicas
-    if (!imagem) {
-      alert("É necessário selecionar uma imagem.");
+const gerarScript = () => {
+  // Verificações iniciais (espaços em branco nas cores, contraste, etc.)
+  for (const campoId in camposComEspaco) {
+    const valorCampo = eval(campoId).trim();
+    if (valorCampo && valorCampo.includes(" ")) {
+      alert(
+        `O campo ${camposComEspaco[campoId]} não pode conter espaços em branco.`,
+      );
       return;
     }
+  }
 
-    if (!numeroAcao) {
-      alert("É necessário informar o número de ação.");
-      return;
-    }
+  // Validar contraste entre cores
+  if (!verificarContrasteCores(corInicio, corTitulo, "título")) return;
+  if (!verificarContrasteCores(corFim, corTitulo, "título")) return;
+  if (!verificarContrasteCores(corInicio, corSubtitulo, "subtítulo")) return;
+  if (!verificarContrasteCores(corFim, corSubtitulo, "subtítulo")) return;
+  if (!verificarContrasteCores(corFundoCTA, corTextoCTA, "texto CTA")) return;
 
-    // Preparar valores para o script
-    let idCATFinal = ID;
-    let codigoFinal = codigo;
-    let metodo = "";
-    let linkValue = "";
+  // Validações básicas
+  if (!imagem) {
+    alert("É necessário selecionar uma imagem.");
+    return;
+  }
 
-    if (tipoLink === "1") {
-      // Sem redirecionamento
-      codigoFinal = "";
-      idCATFinal = "0";
-      metodo = "";
-    } else if (tipoLink === "2") {
-      // Link
-      idCATFinal = "0";
-      metodo = "Link";
-      linkValue = link || "";
+  if (!numeroAcao) {
+    alert("É necessário informar o número de ação.");
+    return;
+  }
 
-      if (!link) {
-        alert("É necessário informar um link de redirecionamento.");
-        setStatusArquivo("Erro ao gerar script: falta link");
-        setStatusArquivoCor("red");
-        return;
-      }
-    } else if (tipoLink === "3") {
-      // Push deep link
-      codigoFinal = "";
-      metodo = "PshDpLink";
+  // Modificar esta parte para usar .then() e .catch() adequadamente
+  verificarResolucaoImagem(imagem)
+    .then(() => {
+      // Preparar valores para o script
+      let idCATFinal = ID;
+      let codigoFinal = codigo;
+      let metodo = "";
+      let linkValue = "";
 
-      if (!idCATFinal) {
-        alert("É necessário informar um ID de redirecionamento.");
-        setStatusArquivo("Erro ao gerar script: falta ID");
-        setStatusArquivoCor("red");
-        return;
-      }
-    }
+      if (tipoLink === "1") {
+        // Sem redirecionamento
+        codigoFinal = "";
+        idCATFinal = "0";
+        metodo = "";
+      } else if (tipoLink === "2") {
+        // Link
+        idCATFinal = "0";
+        metodo = "Link";
+        linkValue = link || "";
 
-    if (idCATFinal === "manual") {
-      idCATFinal = IDManual;
-    }
-
-    if (codigoFinal === "manual") {
-      codigoFinal = codigoManual;
-    }
-
-    // Definir tamanhos para o modelo JSON
-    let setTamanhoTitulo = "1";
-    if (tamanhoTitulo === "50") setTamanhoTitulo = "2";
-    else if (tamanhoTitulo === "65") setTamanhoTitulo = "3";
-
-    let setTamanhoSubtitulo = "1";
-    if (tamanhoSubtitulo === "28") setTamanhoSubtitulo = "2";
-    else if (tamanhoSubtitulo === "32") setTamanhoSubtitulo = "3";
-
-    setStatusArquivo("Gerando script...");
-    setStatusArquivoCor("blue");
-
-    const reader = new FileReader();
-    reader.onload = function (e) {
-      const imageBase64 = e.target.result.split(",")[1]; // Remover o prefixo data:image/xyz;base64,
-
-      // Buscar o modelo JSON
-      fetch("/modeloPopUp.json")
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error(
-              `Erro ao carregar modeloPopUp.json: ${response.status}`,
-            );
-          }
-          return response.json();
-        })
-        .then((modeloJson) => {
-          // Substituir os valores no template
-          let script = modeloJson.script
-            .replace("${ImagemEmBase64}", imageBase64)
-            .replace(/\${numeroAcao}/g, numeroAcao)
-            .replace(/\${titulo}/g, titulo)
-            .replace(/\${corInicio}/g, corInicio)
-            .replace(/\${corFim}/g, corFim)
-            .replace(/\${corTitulo}/g, corTitulo)
-            .replace(/\${subtitulo}/g, subtitulo)
-            .replace(/\${corSubtitulo}/g, corSubtitulo)
-            .replace(/\${textoCTA}/g, textoCTA)
-            .replace(/\${link}/g, linkValue)
-            .replace(/\${codigo}/g, codigoFinal)
-            .replace(/\${corTextoCTA}/g, corTextoCTA)
-            .replace(/\${corFundoCTA}/g, corFundoCTA)
-            .replace(/\${corBordaCTA}/g, corBordaCTA)
-            .replace(/\${tipoLayout}/g, tipoLayout)
-            .replace("${idCAT}", idCATFinal)
-            .replace("${metodo}", metodo)
-            .replace("${tamanhotitulo}", setTamanhoTitulo)
-            .replace("${tamanhosubtitulo}", setTamanhoSubtitulo)
-            .replace("${textoBotaoFechar}", textoBtnFechar)
-            .replace("${corBotaoFechar}", corBtnFechar);
-
-          // Criar e baixar o arquivo
-          const blob = new Blob([script], { type: "text/plain" });
-          const downloadLink = document.createElement("a");
-          downloadLink.href = window.URL.createObjectURL(blob);
-          downloadLink.download = `script_popup_${numeroAcao}.txt`;
-          document.body.appendChild(downloadLink);
-          downloadLink.click();
-          document.body.removeChild(downloadLink);
-
-          setStatusArquivo(`Script gerado com sucesso! Download iniciado.`);
-          setStatusArquivoCor("green");
-        })
-        .catch((error) => {
-          console.error("Erro:", error);
-          setStatusArquivo(`Erro ao gerar script: ${error.message}`);
+        if (!link) {
+          alert("É necessário informar um link de redirecionamento.");
+          setStatusArquivo("Erro ao gerar script: falta link");
           setStatusArquivoCor("red");
-        });
-    };
+          return;
+        }
+      } else if (tipoLink === "3") {
+        // Push deep link
+        codigoFinal = "";
+        metodo = "PshDpLink";
 
-    reader.readAsDataURL(imagem);
-  };
+        if (!idCATFinal) {
+          alert("É necessário informar um ID de redirecionamento.");
+          setStatusArquivo("Erro ao gerar script: falta ID");
+          setStatusArquivoCor("red");
+          return;
+        }
+      }
+
+      if (idCATFinal === "manual") {
+        idCATFinal = IDManual;
+      }
+
+      if (codigoFinal === "manual") {
+        codigoFinal = codigoManual;
+      }
+
+      // Definir tamanhos para o modelo JSON
+      let setTamanhoTitulo = "1";
+      if (tamanhoTitulo === "50") setTamanhoTitulo = "2";
+      else if (tamanhoTitulo === "65") setTamanhoTitulo = "3";
+
+      let setTamanhoSubtitulo = "1";
+      if (tamanhoSubtitulo === "28") setTamanhoSubtitulo = "2";
+      else if (tamanhoSubtitulo === "32") setTamanhoSubtitulo = "3";
+
+      setStatusArquivo("Gerando script...");
+      setStatusArquivoCor("blue");
+
+      const reader = new FileReader();
+      reader.onload = function (e) {
+        const imageBase64 = e.target.result.split(",")[1];
+
+        // Buscar o modelo JSON e continuar com a geração do script
+        fetch("/modeloPopUp.json")
+          .then((response) => {
+            if (!response.ok) {
+              throw new Error(
+                `Erro ao carregar modeloPopUp.json: ${response.status}`,
+              );
+            }
+            return response.json();
+          })
+          .then((modeloJson) => {
+            // Substituir os valores no template
+            let script = modeloJson.script
+              .replace("${ImagemEmBase64}", imageBase64)
+              .replace(/\${numeroAcao}/g, numeroAcao)
+              .replace(/\${titulo}/g, titulo)
+              .replace(/\${corInicio}/g, corInicio)
+              .replace(/\${corFim}/g, corFim)
+              .replace(/\${corTitulo}/g, corTitulo)
+              .replace(/\${subtitulo}/g, subtitulo)
+              .replace(/\${corSubtitulo}/g, corSubtitulo)
+              .replace(/\${textoCTA}/g, textoCTA)
+              .replace(/\${link}/g, linkValue)
+              .replace(/\${codigo}/g, codigoFinal)
+              .replace(/\${corTextoCTA}/g, corTextoCTA)
+              .replace(/\${corFundoCTA}/g, corFundoCTA)
+              .replace(/\${corBordaCTA}/g, corBordaCTA)
+              .replace(/\${tipoLayout}/g, tipoLayout)
+              .replace("${idCAT}", idCATFinal)
+              .replace("${metodo}", metodo)
+              .replace("${tamanhotitulo}", setTamanhoTitulo)
+              .replace("${tamanhosubtitulo}", setTamanhoSubtitulo)
+              .replace("${textoBotaoFechar}", textoBtnFechar)
+              .replace("${corBotaoFechar}", corBtnFechar);
+
+            // Criar e baixar o arquivo
+            const blob = new Blob([script], { type: "text/plain" });
+            const downloadLink = document.createElement("a");
+            downloadLink.href = window.URL.createObjectURL(blob);
+            downloadLink.download = `script_popup_${numeroAcao}.txt`;
+            document.body.appendChild(downloadLink);
+            downloadLink.click();
+            document.body.removeChild(downloadLink);
+
+            setStatusArquivo(`Script gerado com sucesso! Download iniciado.`);
+            setStatusArquivoCor("green");
+          })
+          .catch((error) => {
+            console.error("Erro:", error);
+            setStatusArquivo(`Erro ao gerar script: ${error.message}`);
+            setStatusArquivoCor("red");
+          });
+      };
+
+      reader.readAsDataURL(imagem);
+    })
+    .catch((error) => {
+      // A rejeição da Promise já definiu o statusArquivo e mostrou o alerta
+      console.error("Erro na validação da imagem:", error);
+      // Não fazemos nada adicional aqui, pois os alertas e mensagens
+      // já foram exibidos na função verificarResolucaoImagem
+    });
+};
 
   return (
     <div className={styles.contentContainer}>
@@ -987,32 +1127,11 @@ const PopupCreator = () => {
           {/* Botão de Ação */}
           <div className={styles.actionButtonContainer}>
             <button
-              type="reset"
-              className={styles.btnReset}
-              onClick={handleReset}
-            >
-              Limpar
-            </button>
-            <button
               type="button"
               id="btnGerarScript"
               className={styles.btnGerarScript}
               onClick={gerarScript}
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className={styles.downloadIcon}
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                fill="none"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"
-                />
-              </svg>
               Baixar Script
             </button>
           </div>
@@ -1021,10 +1140,6 @@ const PopupCreator = () => {
 
       {/* Preview */}
       <div className={styles.previewSection}>
-        <p className={styles.previewStatus} style={{ color: statusArquivoCor }}>
-          {statusArquivo}
-        </p>
-
         {/* Mockup do telefone */}
         <div className={styles.phoneMockup}>
           <div className={styles.phoneNotch}></div>
