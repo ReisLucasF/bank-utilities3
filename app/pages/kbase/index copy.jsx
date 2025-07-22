ca8ddda25bd6f8a9c4c3af6011c4f4bc464c057e
ReisLucasF <reislucasf@icloud.com>
2025-07-22 11:06:54 -0300

    indispon knowlageBase

--- app/pages/kbase/index copy.jsx
+++ app/pages/kbase/index copy.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import KnowledgeBase from "/components/KnowledgeBase";
 import styles from "/styles/KnowledgeBase.module.css";
 
 export default function CardCreatorPage() {
@@ -7,13 +8,7 @@ export default function CardCreatorPage() {
       <div className={styles.pageHeader}>
         <h1 className={styles.pageTitle}>Base de Conhecimento</h1>
       </div>
-      <div className={styles.unavailableMessage}>
-        <h2>Página Temporariamente Indisponível</h2>
-        <p>
-          Estamos trabalhando para implementar a Base de Conhecimento
-          diretamente dentro do GIB (Gestão Internet Banking).
-        </p>
-      </div>
+      <KnowledgeBase />
     </div>
   );
 }
